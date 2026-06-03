cd9fdf02aef252c3fa38d364a5b1f6ab69990f45
marques0105-beep <joao.goncalomarques0105@gmail.com>
2026-06-03 18:01:23 +0100

    alteracoes

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    // Carrega o ficheiro principal de interface QML
     const QUrl url(u"qrc:/Main.qml"_qs);
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+        
     engine.load(url);
 
     return app.exec();
--- passenger.h
+++ passenger.h
@@ -7,11 +7,13 @@
 class Passenger {
 public:
     Passenger() = default;
+    // Construtor que define a cor do passageiro 
     explicit Passenger(const QString& col) : color(col) {}
 
+    // Atributo de cor do passageiro
     QString color;
 
-    // For QML display
+    // Método para converter o passageiro em um mapa de variantes
     QVariantMap toVariantMap() const {
         QVariantMap map;
         map["color"] = color;
